213db0d451e6996832d590fe4aa7648b1605340a
KaiKaiKay <candy168741@gmail.com>
2025-10-11 00:16:21 +0800

    add: new pages and Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WeatherListComponent } from './views/weather-list/weather-list.component';
+import { FavoritesComponent } from './views/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'weatherList' },
+  { path: 'weatherList', component: WeatherListComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '**', redirectTo: 'weatherList' }
+];
+
