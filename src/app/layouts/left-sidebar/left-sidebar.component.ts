1144ffba60f67b325e910184d043ce0868765df8
KaiKaiKay <candy168741@gmail.com>
2025-10-11 02:16:12 +0800

    update: side bar Nav Link
    渲染路由項目

--- src/app/layouts/left-sidebar/left-sidebar.component.ts
+++ src/app/layouts/left-sidebar/left-sidebar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterModule } from '@angular/router';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,RouterLinkActive, NgClass],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.scss'
 })
 export class LeftSidebarComponent {
-
+  navItems = [
+    {
+      routeLink: 'weatherList',
+      icon: 'a-solid fa-cloud-sun',
+      label: '氣象查詢'
+    },
+    {
+      routeLink: 'favorites',
+      icon: 'a-solid fa-star',
+      label: '我的最愛'
+    }
+  ];
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()], // 提供 HttpClient 服務
+  providers: [provideHttpClient(),provideRouter(routes)], // 提供 HttpClient 服務
 });
