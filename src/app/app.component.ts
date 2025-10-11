f3ba91cc5fb86541fec25fe95a0661066656cc0d
KaiKaiKay <candy168741@gmail.com>
2025-10-12 01:52:50 +0800

    update: app.component.html 使用側邊欄  主體內容區 設定
    監聽視窗大小判斷是否折疊側邊欄
    主體內容區根據視窗寬度選擇 CSS 類別

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeftSidebarComponent } from './layouts/left-sidebar/left-sidebar.component';
 import { MainComponent } from './layouts/main/main.component';
@@ -6,10 +6,33 @@ import { MainComponent } from './layouts/main/main.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LeftSidebarComponent,MainComponent],
+  imports: [RouterOutlet, LeftSidebarComponent, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'weather-web';
+  isLeftSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize') //@HostListener 監聽視窗大小改變事件
+
+  /** 當視窗大小改變 就更新 screenWidth 的值 
+   * 如果視窗寬度小於 768 像素
+   * 就將 isLeftSidebarCollapsed 設置為 true（折疊側邊欄）*/
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
+  }
+
+  /** 初始化時 判斷側邊欄是否折疊 */
+  ngOnInit(): void {
+    this.isLeftSidebarCollapsed.set(this.screenWidth() < 768); 
+  }
+
+  /** 接收 LeftSidebarComponent 事件，更新 isLeftSidebarCollapsed 的值 */
+  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
+    this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed); 
+  }
 }
--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  /** 是否折疊側邊欄 */
+  isLeftSidebarCollapsed = input.required<boolean>();
+  
+  /** 視窗寬度 */
+  screenWidth = input.required<number>();
 
+  /** 根據視窗寬度選擇 CSS */
+  sizeClass = computed(() => {
+    const isLeftSidebarCollapsed = this.isLeftSidebarCollapsed();
+    if (isLeftSidebarCollapsed) {
+      return '';
+    }
+    return this.screenWidth() > 768 ? 'body-trimmed' : 'body-md-screen';
+  });
 }
