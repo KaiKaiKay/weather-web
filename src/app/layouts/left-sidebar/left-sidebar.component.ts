3de9f51b0aa93177f03c55c97b8df09b5f50d7d8
KaiKaiKay <candy168741@gmail.com>
2025-10-12 01:31:29 +0800

    update: 側邊欄切版
    icon要加fa-solid才會顯示
    建立開關/縮放功能
    scss樣式建立

--- src/app/layouts/left-sidebar/left-sidebar.component.ts
+++ src/app/layouts/left-sidebar/left-sidebar.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RouterLinkActive, RouterModule } from '@angular/router';
-import { NgClass } from "@angular/common";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [RouterModule,RouterLinkActive, NgClass],
+  imports: [RouterModule, RouterLinkActive, NgClass],
   templateUrl: './left-sidebar.component.html',
-  styleUrl: './left-sidebar.component.scss'
+  styleUrl: './left-sidebar.component.scss',
 })
 export class LeftSidebarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
   navItems = [
     {
       routeLink: 'weatherList',
-      icon: 'a-solid fa-cloud-sun',
-      label: '氣象查詢'
+      icon: 'fal fa-solid fa-cloud-sun',
+      label: '氣象查詢',
     },
     {
       routeLink: 'favorites',
-      icon: 'a-solid fa-star',
-      label: '我的最愛'
-    }
+      icon: 'fal fa-solid  fa-star',
+      label: '我的最愛',
+    },
   ];
+
+  toggleCollapse(): void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
 }
